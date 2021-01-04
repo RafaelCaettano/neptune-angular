b3398f6fa3f100f8090455bc47540b2eb8500bf6
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-01-04 13:57:24 -0300

    Atualizando o componente Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Planet } from '../shared/models/planet.model';
+import { PlanetService } from '../shared/services/planets.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css'],
+	providers: [PlanetService]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	planets: Planet[];
+	private planetService: PlanetService;
 
-  ngOnInit(): void {
-  }
+	constructor(planetService: PlanetService) {
+		this.planetService = planetService;
+	}
+
+	ngOnInit(): void {
+
+		// this.planetService.getPlanets().subscribe((planets: Planet[]) => {
+		// 	this.planets = planets
+		// })
+
+	}
 
 }
