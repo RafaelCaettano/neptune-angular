956b30e860afe1b05cb93d3315a070da6bc379f0
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-01-14 12:12:20 -0300

    Atualizando o AuthGuard

--- src/app/shared/auth/guard/auth.guard.ts
+++ src/app/shared/auth/guard/auth.guard.ts
@@ -13,8 +13,11 @@ export class AuthGuard implements CanActivate {
 	}
 
 	canActivate(
-		route: ActivatedRouteSnapshot,
-		state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+		next: ActivatedRouteSnapshot,
+		state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+		if (this.authService.isLoggedIn !== true) {
+			this.router.navigate(['sign-in'])
+		}
 		return true;
 	}
 
