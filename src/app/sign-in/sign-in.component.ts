aef79af9c9bc4f7339cc71b2548f428c03a464ae
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-01-14 11:56:03 -0300

    Atualizando o componente SignIn

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { AuthService } from '../shared/auth/auth.service';
 
 @Component({
-  selector: 'app-sign-in',
-  templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+	selector: 'app-sign-in',
+	templateUrl: './sign-in.component.html',
+	providers: [AuthService],
+	styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+	form: FormGroup;
+	authService: AuthService
 
-  ngOnInit(): void {
-  }
+	constructor(private fb: FormBuilder, authService: AuthService) { 
+		this.authService = authService;
+	}
+
+	ngOnInit(): void {
+
+		this.form = this.fb.group({
+			email: new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(120), Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$')]),
+			password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+		})
+
+	}
 
 }
