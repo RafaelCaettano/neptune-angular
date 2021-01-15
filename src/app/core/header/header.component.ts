33357ba0c4206e37df9f72798002183b92c2b978
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-01-15 11:05:17 -0300

    Atualizando o componente Header

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { User } from 'src/app/shared/models/user.model';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css'],
+	providers: [AuthService, UserService],
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+	authService: AuthService;
+	userService: UserService;
+	user: User;
 
-  ngOnInit(): void {
-  }
+	constructor(authService: AuthService, userService: UserService) { 
+		this.authService = authService;
+		this.userService = userService;
+	}
+
+	ngOnInit(): void {
+
+		this.user = new User;
+		this.userService.emailRegistered(this.authService.getEmail).subscribe(
+			(user: User[]) => {
+				this.user = user[0];
+				console.log(this.user)
+			}
+		)
+
+	}
 
 }
