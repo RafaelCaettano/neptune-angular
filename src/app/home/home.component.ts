22bc437d9d8565f0c2a4dcaf130ef21a023f2c2e
RafaelCaettano <rafac.docarmo@hotmail.com>
2021-01-15 12:03:20 -0300

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,30 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/auth/auth.service';
-import { Planet } from '../shared/models/planet.model';
-import { PlanetService } from '../shared/services/planets.service';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
 	styleUrls: ['./home.component.css'],
-	providers: [PlanetService, AuthService]
+	providers: [AuthService]
 })
 export class HomeComponent implements OnInit {
 
-	planets: Planet[];
-	planetService: PlanetService;
 	authService: AuthService;
 
-	constructor(planetService: PlanetService, authService: AuthService) {
-		this.planetService = planetService;
+	constructor(authService: AuthService) {
 		this.authService = authService;
 	}
 
 	ngOnInit(): void {
-
-		this.planetService.getPlanets().subscribe((planets: Planet[]) => {
-		this.planets = planets
-		})
 
 	}
 
